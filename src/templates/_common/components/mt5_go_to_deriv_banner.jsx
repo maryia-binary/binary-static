616c35f5131b1d1eb0f574ba2dd21b3459a68c42
maryia-binary <maryia@binary.com>
2022-07-21 13:12:30 +0300

    finilized Mt5GoToDerivBanner styling (desktop+responsive)

--- src/templates/_common/components/mt5_go_to_deriv_banner.jsx
+++ src/templates/_common/components/mt5_go_to_deriv_banner.jsx
@@ -7,13 +7,11 @@ const Mt5GoToDerivBanner = ({ has_margin }) => {
         <div id={`${classname}_container`} className={`invisible ui-helper-clearfix ${has_margin ? 'has_margin' : ''}`}>
             <a id={`${classname}-link`} target='_blank' rel='noopener noreferrer' href='#'>
                 <img className={`${classname}_icons`} src={it.url_for('images/mt5_banner/mt5_go_to_deriv_banner_icons.svg')} />
-                <div>
+                <div className={`${classname}_text`}>
                     <h3>{it.L('Go to Deriv to add an MT5 account')}</h3>
                     <p>{it.L('You\'ll be able to log in to Deriv using your Binary.com credentials.')}</p>
                 </div>
-                <div>
-                    <div className={`${classname}_btn`}>{it.L('Go to Deriv')}</div>
-                </div>
+                <div className={`${classname}_btn`}>{it.L('Go to Deriv')}</div>
             </a>
         </div>
     );
